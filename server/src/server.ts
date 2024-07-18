764cd9b6e677814ac762264cf37066e973635a82
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-07-18 19:46:33 -0300

    feat: implement cors on apiDDD

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,17 @@
 import "express-async-errors";
 import Express from "express";
+import cors from "cors";
 import { router } from "./router";
 import { errorMiddleware } from "./middleware/error-middleware";
 import { db } from "./db/db";
+import { corsMiddleware } from "./middleware/cors-middleware";
 
 const PORT = 8080;
 
 const app = Express();
 app.use(Express.json());
+
+app.use(corsMiddleware, cors());
 
 app.use(router);
 
